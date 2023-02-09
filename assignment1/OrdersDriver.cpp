120bb4fe408a3ae3c4085afc27455f1661ee2016
Samuel Chuang <samuelchuang98@gmail.com>
2023-02-09 10:09:21 -0500

    Added Orders

---
+++ assignment1/OrdersDriver.cpp
@@ -0,0 +1,50 @@
+#include "Orders.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "test OrderDriver...\n" << endl;
+	OrderList ordersList;
+
+	Deploy aDeploy;
+	Advance anadvance;
+	Bomb abomb;
+	Blockade ablockade;
+	Airlift anairlift;
+	Negotiate anegotiate;
+
+	ordersList.set_order_list(&aDeploy);
+	ordersList.set_order_list(&anadvance);
+	ordersList.set_order_list(&abomb);
+	ordersList.set_order_list(&ablockade);
+	aordersList.set_order_list(&anairlift);
+	ordersList.set_order_list(&anegotiate);
+
+
+	//print orderlist
+	cout << "\n the orderlist contains: " << endl;
+	for (int i = 0; i < ordersList.get_order_list()->size(); i++) {
+		cout <<"  "<< ordersList.get_order_list()->at(i)->get_type() << endl;
+	}
+
+	//delete an order
+	ordersList.delete_order(&aDeploy);
+
+	//print orderlist
+	cout << "\n the orderlist contains: " << endl;
+	for (int i = 0; i < ordersList.get_order_list()->size(); i++) {
+		cout << "  " << ordersList.get_order_list()->at(i)->get_type() << endl;
+	}
+
+	//move an order
+	ordersList.move(0,4);
+	//ordersList.move(0, 5);
+	ordersList.move(0, 8);//invalid
+	//print orderlist
+	cout << "\n the orderlist contains: " << endl;
+	for (int i = 0; i < ordersList.get_order_list()->size(); i++) {
+		cout << "  " << ordersList.get_order_list()->at(i)->get_type() << endl;
+	}
+
+}
