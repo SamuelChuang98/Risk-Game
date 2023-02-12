3949fed4e82c730254c9ee81179b1e45a7ee87d0
Samuel Chuang <samuelchuang98@gmail.com>
2023-02-12 17:22:04 -0500

    Fixed move()

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -4,11 +4,11 @@
 //#include "Cards.h"
 //#include "Player.h"
 #include "Orders.h"
-#include "Map.h"
+//#include "Map.h"
 
 int main() {
     //cardSpace::cardsMain();
     //playerSpace::playerMain();
     orderSpace::orderMain();
-    mapSpace::mapMain();
+    //mapSpace::mapMain();
 }
