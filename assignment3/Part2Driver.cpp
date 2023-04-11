a462835c2f12137a1e5cc745533888420e2dd019
nicholaszako <nicholas.zako@hotmail.com>
2023-04-11 16:17:56 -0400

    Add program argument support to driver

--- assignment3/Part2Driver.cpp
+++ assignment3/Part2Driver.cpp
@@ -12,9 +12,26 @@ int main(int argc, char *argv[]) {
     GameEngine game = GameEngine(&gs);
 
     CommandProcessor* cmdProc;
-    cmdProc = new CommandProcessor(gs);
 
-    // Get every command...
+    // Will assign a CommandProcessor or FileCommandProcessorAdapter
+    // depending on command-line arguments
+    for(int i = 0; ; i++){
+        if (strcmp(argv[i], "-console") == 0){
+            cmdProc = new CommandProcessor(gs);
+            break;
+        }
+            // If at last arg, -file is not possible. Default to console.
+        else if (i == argc - 1){
+            cmdProc = new CommandProcessor(gs);
+            break;
+        }
+        else if ((strcmp(argv[i], "-file") == 0)){
+            cmdProc = new FileCommandProcessorAdapter(gs, argv[i + 1]);
+            break;
+        }
+    }
+
+    // Get first command - Assumes there is only one command
     Command c = cmdProc->getCommand();
 
     if (!c.tournament.maps.empty()){
